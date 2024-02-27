97f270f1395b068a6d3c92e1e13c7d54097c96f2
Junior <juniorrr123321@gmail.com>
2024-02-26 19:53:42 -0500

    actualizando update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -40,7 +40,7 @@ const update = catchError(async(req, res) => {
         password: password,
         birthday: birthday
     }, { where: { id: id}, returning: true});
-    return res.json(user);
+    return res.json(user[1][0]);
 });
 
 
